05bd1b9678a1a0fea521fed399c5bad768fc16ae
randombishop <nabil.abdellaoui@expr-research.com>
2024-07-16 23:06:21 -0400

    AppBar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,21 +7,40 @@ import DailyDigest from './components/DailyDigest';
 import LikeMeter from './components/LikeMeter';
 import Clusters from './components/Clusters';
 import Profile from './components/Profile';
+import AppLogo from './assets/logo_white_nobg.png';
+
+
+
 
 class App extends React.Component {
   render() {
+    const imgStyle = { 
+      height: '50px', 
+      width: 'auto' 
+    } ;
+    const titleStyle = {
+      marginLeft: '50px',
+      marginRight: '50px', 
+      fontWeight: 'bold', 
+      color: 'white',
+      textShadow: '2px 2px 4px #318787',
+      lineHeight: '24px',
+      textAlign: 'center' as const
+    } ;
     return (
       <Router>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6" style={{ flexGrow: 1 }}>
-              Cyberpunk App
+            <img src={AppLogo} style={imgStyle} />
+            <Typography variant="h6" style={titleStyle}>
+              Farcaster<br/>Data
             </Typography>
             <Button color="inherit" href="/">Dashboard</Button>
             <Button color="inherit" href="/trends">Trends</Button>
             <Button color="inherit" href="/daily-digest">Daily Digest</Button>
             <Button color="inherit" href="/like-meter">Like-meter</Button>
             <Button color="inherit" href="/clusters">Clusters</Button>
+            <div style={{ flexGrow: 1 }}></div>
             <Profile />
           </Toolbar>
         </AppBar>
