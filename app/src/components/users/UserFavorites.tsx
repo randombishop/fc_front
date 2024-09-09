35c928b6ce75de676e1345214f0f14bdd9707ab8
randombishop <nabil.abdellaoui@expr-research.com>
2024-09-09 09:05:10 -0400

    fav users

--- app/src/components/users/UserFavorites.tsx
+++ app/src/components/users/UserFavorites.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import Panel from '../common/Panel'; 
 
 
@@ -6,12 +7,32 @@ class UserFavorites extends React.Component< {data: any}> {
   
   render() {
     const data = this.props.data ;
-    if (!data) {
+    if (!data || !data.favoriteUsers) {
       return null ;
     }
+    const favoriteUsers = data.favoriteUsers ;
     return (
       <Panel title="Favorite Users">
-        hello
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>User Name</TableCell>
+              <TableCell align="right">Recasts</TableCell>
+              <TableCell align="right">Likes</TableCell>
+              <TableCell align="right">Replies</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {favoriteUsers.map((user: any) => (
+              <TableRow key={user.target_fid}>
+                <TableCell>{user.username}</TableCell>
+                <TableCell align="right">{user.num_recasts}</TableCell>
+                <TableCell align="right">{user.num_likes}</TableCell>
+                <TableCell align="right">{user.num_replies}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Panel>
     );
   }
--- app/src/components/users/UserTopFollowers.tsx
+++ app/src/components/users/UserTopFollowers.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Table, TableBody, TableCell, TableRow } from '@mui/material';
 import Panel from '../common/Panel'; 
 
 
@@ -6,12 +7,24 @@ class UserTopFollowers extends React.Component< {data: any}> {
   
   render() {
     const data = this.props.data ;
-    if (!data) {
+    if (!data || !data.topFollowers) {
       return null ;
     }
+    const topFollowers = data.topFollowers ;
     return (
       <Panel title="Top Followers">
-        hello
+        <Table>
+          <TableBody>
+            {topFollowers.map((u: any) => (
+              <TableRow key={u.fid}>
+                <TableCell component="th" scope="row">
+                  {u.user_name}
+                </TableCell>
+                <TableCell align="right">{u.followers_num}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Panel>
     );
   }
